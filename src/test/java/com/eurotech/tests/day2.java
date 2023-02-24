fc59f4f75eefd8bdf2c7955ec3c02fcf1fafdaa0
Erhan <erhanilgar@gmail.com>
2023-02-24 19:15:59 +0000

    Day 2 amazon added

--- src/test/java/com/eurotech/tests/day2.java
+++ src/test/java/com/eurotech/tests/day2.java
@@ -3,7 +3,9 @@ package com.eurotech.tests;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -12,6 +14,7 @@ import java.net.URL;
 
 public class day2 {
     AppiumDriver<MobileElement> driver;
+
     @Test
     public void amazon() throws MalformedURLException, InterruptedException {
 
@@ -29,7 +32,40 @@ public class day2 {
 
         driver = new AppiumDriver<MobileElement>(new URL("http://localhost:4723/wd/hub"),desiredCapabilities);
 
+        //Task
+        //--> Open the Amazon app
+        //--> Click skip sign in button
+        //--> Search "Apple Watch"
+        //--> Click  "Apple Watch Series 7"
+        //--> Verify that Results have existed
+
+        //UI Automation
+        //*[text()='Skip sign in']
+
+
+        //Mobile Testing
+        //*[@text='Skip sign in']
+
         Thread.sleep(2000);
+        MobileElement skipSignBtn = driver.findElement(By.xpath("//*[@text='Skip sign in']"));
+        skipSignBtn.click();
+
+        Thread.sleep(1000);
+        MobileElement searchBox = driver.findElement(By.xpath("//*[@text='Search Amazon']"));
+        Thread.sleep(2000);
+        searchBox.click();
+        searchBox.sendKeys("Apple Watch");
+
+
+        MobileElement watch7 = driver.findElement(By.xpath("//*[@text='apple watch series 7']"));
+        watch7.click();
+        Thread.sleep(2000);
+
+        MobileElement watch7Result = driver.findElement(By.xpath("(//*[@text='Apple Watch Series 7 (GPS, 45MM) - Blue Aluminum Case with Abyss Blue Sport Band (Renewed Premium)'])[2]"));
+        Assert.assertTrue(watch7Result.getText().contains("Apple Watch Series 7"));
+
+
+        Thread.sleep(3000);
 
         driver.closeApp();
 
